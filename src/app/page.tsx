c061710b327558843eb2e5827a3a8e4ee24b1d54
matayo-ayo <mathewfribergayo@gmail.com>
2025-06-10 20:10:40 +0300

    REGIONAL FORM | Region search is using shadcn select component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Header from "../components/header";
-import Location from "./forms/location";
+import Regional from "../components/regional";
 import Postcode from "./forms/postcode";
 
 export default function Home() {
@@ -10,13 +10,13 @@ export default function Home() {
     <div className="min-h-screen">
       <Header />
       <div className="container mx-auto max-w-md px-4 py-8 flex flex-col items-center justify-center min-h-[calc(100vh-50px)]">
-        <Tabs className="w-full" defaultValue="location">
+        <Tabs className="w-full" defaultValue="regional">
           <TabsList className="w-full gap-3 mb-5">
-            <TabsTrigger value="location">Location</TabsTrigger>
+            <TabsTrigger value="regional">Regional</TabsTrigger>
             <TabsTrigger value="postcode">Postcode</TabsTrigger>
           </TabsList>
-          <TabsContent value="location">
-            <Location />
+          <TabsContent value="regional">
+            <Regional />
           </TabsContent>
           <TabsContent value="postcode">
             <Postcode />
